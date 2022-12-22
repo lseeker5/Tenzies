edce17e92eba0ae49fa9da0a6d79b3a04e045c25
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2022-12-22 10:03:27 +0800

    yayay
    yay

---
+++ my-app/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+const[dices,setDices]=React.useState([
+{id:1,locked:false,value:1},
+{id:2,locked:false,value:2},
+{id:3,locked:false,value:3},
+{id:4,locked:false,value:4},
+{id:5,locked:false,value:5},
+{id:6,locked:false,value:6},
+{id:7,locked:false,value:1},
+{id:8,locked:false,value:2},
+{id:9,locked:false,value:3},
+{id:10,locked:false,value:4}
+])
+
+const Dices=dices.map(createDice)
+
+function handleClick(num){
+  const newDices=[...dices]
+  for(let i=0;i<newDices.length;i++){
+    if(newDices[i].id===num){
+      const currentValue=newDices[i].locked
+      newDices[i].locked=!currentValue
+    }
+  }
+  setDices(newDices)
+}
+
+function createDice(val){
+  const id=val.id
+  const locked=val.locked
+  const value=val.value
+  const ClassName=locked?"locked-dice":"unlocked-dice"
+  return(
+    <div 
+    className={ClassName}
+    id={id}
+    locked={locked}
+    onClick={()=>{handleClick(id)}}
+    > 
+    {value}
+    
+    </div>
+  )
+}
+
+  return (
+    <div className="App">
+      <h1>TENZIES</h1>
+      <div className='allThemDices'>{Dices}</div>
+      
+    </div>
+  );
+}
+
+export default App;
