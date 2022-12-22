1f430b8c0fc5f3bd3b8ecbba1a5d32c1dde37367
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2022-12-22 10:54:45 +0800

    hehe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,7 +17,23 @@ const[dices,setDices]=React.useState([
 
 const[count,setCount]=React.useState(0)
 
+const[victory,setVictory]=React.useState(false)
+
 const Dices=dices.map(createDice)
+
+React.useEffect(()=>{
+let firstNum=dices[1].value
+let same=true
+for(let i=1;i<Dices.length;i++){
+  if(dices[i].value!==firstNum){
+    same=false
+    break
+  }
+}
+if(same){
+  setVictory(true)
+}
+},[count])
 
 function handleClick(num){
   const newDices=[...dices]
@@ -36,12 +52,30 @@ function rollDice(){
   const newDices=[...dices]
   for(let i=0;i<newDices.length;i++){
     if(newDices[i].locked===false){
-      let newNum =Math.floor(6*Math.random())  
+      let newNum =Math.ceil(6*Math.random())  
       newDices[i].value=newNum
     }
   }
   setDices(newDices)
   setCount(prevCount=>prevCount+1)
+}
+
+function resetDice(){
+  setDices([
+    {id:1,locked:false,value:1},
+    {id:2,locked:false,value:2},
+    {id:3,locked:false,value:3},
+    {id:4,locked:false,value:4},
+    {id:5,locked:false,value:5},
+    {id:6,locked:false,value:6},
+    {id:7,locked:false,value:1},
+    {id:8,locked:false,value:2},
+    {id:9,locked:false,value:3},
+    {id:10,locked:false,value:4}
+    ])
+
+    setCount(0)
+    setVictory(false)
 }
 
 function createDice(val){
@@ -66,8 +100,12 @@ function createDice(val){
     <div className="App">
       <h1>TENZIES</h1>
       <div className='allThemDices'>{Dices}</div>
-      <div className='count'>{count}</div>
+      <div className='count'>Number of Rolls:{count}</div>
+      <div className='buttons'>
       <button className='roll' onClick={rollDice}>ROLL</button>
+      <button className='reset' onClick={resetDice}>RESET</button>
+      </div>
+      {victory&&<h2>YOU WIN</h2>}
     </div>
   );
 }
